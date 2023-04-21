de434a9b790fce6469a728d3b7d0e7b7ac87b024
TiagoMPereira <tiagodemoraispereira@gmail.com>
2023-04-21 14:17:55 -0300

    feat: setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+setuptools.setup(
+    name='movie_engine',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    author='morales',
+    author_email='tiago.pereira@gec.inatel.br',
+    description='Movies recomender package',
+    install_requires=required,
+    # package_data={'':['*.csv', '*.txt', '*.pkl']},
+    include_package_data=True
+)
